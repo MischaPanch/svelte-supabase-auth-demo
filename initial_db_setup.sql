9771e0e677db61f63679748ede83d4b90ef3f0c1
Michael Panchenko <michael.panchenko@oraios-ai.de>
2025-01-01 23:09:38 +0100

    Initial sql command

---
+++ initial_db_setup.sql
@@ -0,0 +1,7 @@
+-- The default postgres user in supabase is not a superuser, so prisma commands
+-- like db push or migrate will fail. 
+-- If you want to manage your database with prisma, 
+-- you need to make the postgres user a superuser by running the following command 
+-- (for example, in the supabase dashboard SQL editor):
+alter role postgres superuser
+initial_sql_commands
